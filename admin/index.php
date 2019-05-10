44b8da26fbcfea2bc2dc373340aaf2d3ed4da996
Michael Admin <mik-ivantsov@mail.ru>
2019-05-10 20:53:54 +0300

    Переимовал Title

--- admin/index.php
+++ admin/index.php
@@ -2,12 +2,12 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>Админка</title>
-
+    <title>CMS Админка</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 </head>
 <body>
 <ul>
+
     <li><a href="users.php">Пользователи</a></li>
     <li><a href=""></a></li>
     <li><a href=""></a></li>
